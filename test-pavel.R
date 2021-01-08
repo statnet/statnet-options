81d963b0bf8831bfc1b56902d11cd9fd73d86144
Michal Bojanowski <michal2992@gmail.com>
2021-01-08 13:26:39 +0100

    Testing Pavel's approach

---
+++ test-pavel.R
@@ -0,0 +1,11 @@
+library(testthat)
+
+# Prepend 'lib' library tree
+withr::local_libpaths("lib", action="prefix")
+
+
+
+test_that("it just works", {
+  library(pkgB)
+  expect_true(!identical(sctrl(), list(B1=2, B2=4)))
+})
