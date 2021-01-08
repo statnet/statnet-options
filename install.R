d6d57ada479cd5e13ebeeede0fa5d7070be91fed
Michal Bojanowski <michal2992@gmail.com>
2021-01-08 13:30:15 +0100

    install.R creates dir if needed

--- install.R
+++ install.R
@@ -1,3 +1,4 @@
+if(!dir.exists("lib")) dir.create("lib")
 install.packages("pkgA", lib="lib", repos = NULL, type = "source")
 install.packages("pkgB", lib="lib", repos = NULL, type = "source")
 install.packages("pkgC", lib="lib", repos = NULL, type = "source")
