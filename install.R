684678fefca0ff94d3bf2a2421bdc69de88dd796
Michal Bojanowski <michal2992@gmail.com>
2021-01-08 11:43:45 +0100

    Install script

---
+++ install.R
@@ -0,0 +1,3 @@
+install.packages("pkgA", lib="lib", repos = NULL, type = "source")
+install.packages("pkgB", lib="lib", repos = NULL, type = "source")
+install.packages("pkgC", lib="lib", repos = NULL, type = "source")
